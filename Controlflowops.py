3e6ccd81c7aa440e10d6bcaaa57a40aee651299e
Deepak <deepakms.real@gmail.com>
2018-04-26 00:55:27 +0530

    Add files via upload

---
+++ Controlflowops.py
@@ -0,0 +1,90 @@
+
+# coding: utf-8
+
+# In[4]:
+
+a='1569'
+b=[]
+for i in a:
+	b.append(i)
+print (b)
+    
+    
+
+
+# In[5]:
+
+len(b)
+
+
+# In[9]:
+
+type(int(b[0]))
+
+
+# In[10]:
+
+b[0]+b[1]
+
+
+# In[13]:
+
+2*int((b[0]+b[1]))
+
+
+# In[35]:
+
+for i in range(1000,10000):
+	b=[]
+	for j in str(i):
+        b.append(j)
+    if((2*int(b[0]+b[1]))==int(b[2]+b[3])):
+        if (2*int(b[0])==int(b[3])):
+            if (int(b[1])==int(b[2])):
+                print(i)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
